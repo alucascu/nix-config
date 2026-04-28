ffe0cbd06f73ff15ffd40d2c6e8e1c177808364a
Asher Lucas-Cuddeback <alucascu@proton.me>
2026-04-28 18:45:50 -0400

    Prepare for install to new PC

---
+++ modules/hosts/odysseus/default.nix
@@ -0,0 +1,51 @@
+{inputs, ...}: {
+  flake.modules.nixos.odysseus =
+    {
+      inputs,
+      pkgs,
+      ...
+    }:
+    {
+      imports = [
+        ./_hardware-configuration.nix
+      ]
+      ++ (with inputs.self.modules.nixos; [
+        system-desktop
+        docker
+        gaming
+        alucascu
+      ]);
+
+      networking.hostName = "odysseus";
+      networking.networkmanager.enable = true;
+      networking.wireless.enable = false;
+
+      services.openssh = {
+        enable = true;
+        settings.PermitRootLogin = "no";
+      };
+
+      hardware.nvidia = {
+        open = true;
+        modesetting.enable = true;
+        powerManagement.enable = false;
+      };
+      services.xserver.videoDrivers = ["nvidia"];
+
+      boot.initrd.kernelModules = ["amdgpu"];
+
+      boot = {
+        loader.limine.enable = true;
+        loader.efi.canTouchEfiVariables = true;
+        kernelPackages = pkgs.linuxPackages_latest;
+      };
+
+      home-manager.users.alucascu.imports = [
+        inputs.self.modules.homeManager.gaming
+      ];
+
+      system.stateVersion = "25.11";
+    };
+
+  flake.nixosConfigurations = inputs.self.lib.mkNixos "x86_64-linux" "odysseus";
+}
---
+++ modules/profiles/gaming.nix
@@ -0,0 +1,25 @@
+{ inputs, pkgs, ... }:
+{
+  flake.modules.nixos.gaming =
+    { pkgs, ... }:
+    {
+      hardware.graphics = {
+        enable = true;
+        enable32Bit = true;
+      };
+
+      programs.steam = {
+        enable = true;
+        extraCompatPackages = [ pkgs.proton-ge-bin ];
+      };
+
+      programs.gamemode.enable = true;
+    };
+
+  flake.modules.homeManager.gaming =
+    { pkgs, ... }:
+    {
+      programs.mangohud.enable = true;
+      home.packages = [ pkgs.lutris ];
+    };
+}
