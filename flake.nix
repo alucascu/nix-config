ae0232ae5a16d52e07f7f9889e5b49120f59974c
Asher Lucas-Cuddeback <alucascu@proton.me>
2026-05-13 23:09:44 -0400

    Speed up rebuild and boot
    Two main causes:
    1. Because flakes didn't have "follows", build up to 5 nixpkgs caches.
    Added follows.
    
    2. Rebuild and boot was referencing a swap partition that didn't need to
    exist. Removed swap partition.

--- flake.nix
+++ flake.nix
@@ -1,18 +1,21 @@
 {
-  description = "Asher's nix configuration";
-
+  description = "asher's nix configuration";
   inputs = {
-    # nixpkgs
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-    # home-manager
     home-manager = {
       url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
     flake-parts.url = "github:hercules-ci/flake-parts";
     import-tree.url = "github:vic/import-tree";
-    lazyvim.url = "github:pfassina/lazyvim-nix";
+
+    lazyvim = {
+      url = "github:pfassina/lazyvim-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     plasma-manager = {
       url = "github:nix-community/plasma-manager";
       inputs = {
@@ -20,7 +23,11 @@
         home-manager.follows = "home-manager";
       };
     };
-    globalprotect-openconnect.url = "github:yuezk/GlobalProtect-openconnect";
+
+    globalprotect-openconnect = {
+      url = "github:yuezk/globalprotect-openconnect";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = inputs:
--- modules/hosts/hades/default.nix
+++ modules/hosts/hades/default.nix
@@ -2,6 +2,7 @@
   flake.modules.nixos.hades = {
     inputs,
     pkgs,
+    lib,
     ...
   }: {
     imports =
@@ -17,23 +18,23 @@
         globalprotect
       ]);
 
+    swapDevices = lib.mkForce [];
+
     home-manager.users.alucascu.myConfig.sshKeyName = "hades";
-
-    networking.hostName = "hades";
-    networking.networkmanager.enable = true;
-    networking.wireless.enable = true;
-
+    networking = {
+      hostName = "hades";
+      networkmanager.enable = true;
+      wireless.enable = true;
+    };
     services.openssh = {
       enable = true;
       settings.PermitRootLogin = "no";
     };
-
     boot = {
       loader.limine.enable = true;
       loader.efi.canTouchEfiVariables = true;
       kernelPackages = pkgs.linuxPackages_latest;
     };
-
     system.stateVersion = "25.11";
   };
 
