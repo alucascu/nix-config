78ac070feed5cbe7b402b50779158d365a245c74
Asher Lucas-Cuddeback <alucascu@proton.me>
2026-05-16 17:36:35 -0400

    Add Rust to nevoim config

--- modules/home/neovim/default.nix
+++ modules/home/neovim/default.nix
@@ -6,6 +6,7 @@
       ./_packages.nix
       ./plugins/_conform.nix
       ./plugins/_lsp.nix
+      ./plugins/_rust.nix
       ./plugins/_obsidian.nix
       ./config/_keymaps.nix
       ./config/_options.nix
