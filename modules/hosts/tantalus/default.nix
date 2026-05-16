7fc5f94ef030a28415af4e459ff5418d649d89c1
Asher Lucas-Cuddeback <alucascu@proton.me>
2026-05-15 23:19:28 -0400

    Implement tantalus config

---
+++ modules/hosts/tantalus/default.nix
@@ -0,0 +1,57 @@
+{inputs, ...}: {
+  flake.modules.nixos.tantalus = {
+    inputs,
+    pkgs,
+    lib,
+    ...
+  }: {
+    imports =
+      [
+        ./_hardware-configuration.nix
+      ]
+      ++ (with inputs.self.modules.nixos; [
+        system-desktop
+        docker
+        alucascu
+      ]);
+
+    home-manager.users.alucascu.myConfig.sshKeyName = "tantalus";
+
+    networking = {
+      hostName = "tantalus";
+      networkmanager = {
+        enable = true;
+        wifi.backend = "iwd";
+      };
+    };
+
+    services.openssh = {
+      enable = true;
+      settings.PermitRootLogin = "no";
+    };
+
+
+    boot.initrd.kernelModules = ["amdgpu"];
+
+    boot = {
+      loader.limine.enable = true;
+      loader.efi.canTouchEfiVariables = true;
+      kernelPackages = pkgs.linuxPackages_latest;
+      extraModprobeConfig = ''
+        options mt7925e disable_aspm=1
+      '';
+    };
+
+    environment.sessionVariables = {
+      KWIN_DRM_DEVICES = "/dev/dri/card2";
+    };
+
+    home-manager.users.alucascu.imports = [
+      inputs.self.modules.homeManager.gaming
+    ];
+
+    system.stateVersion = "25.11";
+  };
+
+  flake.nixosConfigurations = inputs.self.lib.mkNixos "x86_64-linux" "tantalus";
+}
