493bf507fe15fb9fb8f466ecf28e8795c3b06287
Asher Lucas-Cuddeback <alucascu@proton.me>
2026-05-17 13:59:09 -0400

    config for tantalus

---
+++ modules/hosts/tantalus/default.nix
@@ -0,0 +1,40 @@
+{inputs, ...}: {
+  flake.modules.nixos.tantalus = {
+    inputs,
+    pkgs,
+    ...
+  }: {
+    imports =
+      [./_hardware-configuration.nix]
+      ++ (with inputs.self.modules.nixos; [
+        system-desktop
+        docker
+        alucascu
+      ]);
+
+    networking.hostName = "tantalus";
+    networking.networkmanager.enable = true;
+
+    services.openssh = {
+      enable = true;
+      settings.PermitRootLogin = "no";
+    };
+
+    boot = {
+      initrd.kernelModules = ["amdgpu"];
+      loader.limine.enable = true;
+      loader.efi.canTouchEfiVariables = true;
+      kernelPackages = pkgs.linuxPackages_latest;
+    };
+
+    hardware.graphics = {
+      enable = true;
+      enable32Bit = true;
+    };
+
+    system.stateVersion = "25.11";
+  };
+
+  flake.nixosConfigurations =
+    inputs.self.lib.mkNixos "x86_64-linux" "tantalus";
+}
