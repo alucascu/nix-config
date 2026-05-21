20aab3662122f547c78eea20795f337d394db592
Asher Lucas-Cuddeback <alucascu@proton.me>
2026-05-21 18:21:52 -0400

    Printer doesn't always exist

--- modules/services/printing.nix
+++ modules/services/printing.nix
@@ -11,19 +11,5 @@
         openFirewall = true;
       };
     };
-
-    hardware = {
-      printers = {
-        ensureDefaultPrinter = "Brother-L2750";
-        ensurePrinters = [
-          {
-            name = "Brother-L2750";
-            location = "Local";
-            deviceUri = "ipp://10.50.30.30/ipp/print";
-            model = "everywhere";
-          }
-        ];
-      };
-    };
   };
 }
